99cf0b198ef115b5d9c03748699f9c938e8cd295
manideep1428 <saimanideep.ch12345@gmail.com>
2025-05-24 13:37:47 +0530

    fixed

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import { motion } from "framer-motion";
 import { Delivery } from "@/components/svgs/delivery";
 import { useSession } from "next-auth/react";
